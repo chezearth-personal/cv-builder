6311852958b3554ac694bf4a31447565c082f04e
Charles Rethman <rethman@me.com>
2024-02-09 14:47:19 +1300

    Trying to get OpenAI past the 429 error

--- server/index.js
+++ server/index.js
@@ -13,7 +13,6 @@ const port = process.env.PORT_KEY || 'Localhost';
 const openAI = new OpenAI({
   apiKey: process.env.OPENAI_API_SECRET_KEY
 });
-// console.log(host, ':', port, randomUUID());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -26,21 +25,23 @@ app.get('/api', (req, res) => {
   });
 });
 
-const GPTFunction = async (text) => {
+const GPTFunction = async (messagesArr) => {
   try {
     const response = await openAI.chat.completions.create({
       model: 'gpt-3.5-turbo-instruct',
-      propmt: text,
-      temperature: 0.6,
-      max_tokens: 250,
-      top_p: 1,
-      frequency_penalty: 1,
-      presence_penatly: 1
+      messages: messagesArr,
+      // temperature: 0.6,
+      // max_tokens: 250,
+      // top_p: 1,
+      // frequency_penalty: 1,
+      // presence_penatly: 1
     });
     return response.data.choices[0].text;
   } catch(err) {
-    if (err) {console.log('err =\n', err)}
-    return 'Error from ChatGPT means we dunno.';
+    if (err) {console.log('err =\n', err.error)}
+    return err && err.error && err.error.message
+      ? `Status: ${err.status}, ${err.error.message}`
+      :'Error from ChatGPT means we dunno.';
   }
 }
 
@@ -57,7 +58,7 @@ const upload = multer({
   limits: { fileSize: 1024 * 1024 * 5 }
 });
 
-const waitForMin = () => new Promise(resolve => setTimeout(resolve, 1000 * 70));
+const waitForMin = async () => new Promise(resolve => setTimeout(resolve, 1000 * 70));
 
 app.post('/cv/create', upload.single('headshotImage'), async (req, res) => {
     const {
@@ -82,36 +83,38 @@ app.post('/cv/create', upload.single('headshotImage'), async (req, res) => {
     const remainderText = workArray
       .reduce((res, e) => res + ` ${e.name} as a ${e.position}.`, '');
     // console.log('remainderText =', remainderText);
-    /** The job description prompt */
-    const prompt1 = `I am writing a CV, my details are `
+    /** The job description message */
+    const message0 = { role: 'system', content: 'You are a helpful assistant'};
+    const message1 = { role: 'user', content: `I am writing a CV, my details are `
       + `\n name: ${fullName} `
       + `\n role: ${currentPosition} (${currentLength} years). `
       + `\n I write in the technologies: ${currentTechnologies}. `
-      + `Can you write a 100 words description for the top of the CV (first person writing)?`;
-    /** The job responsibilities prompt */
-    const prompt2 = `I am writing a CV, my details are `
+      + `Can you write a 100 words description for the top of the CV (first person writing)?` };
+    /** The job responsibilities message */
+    const message2 = { role: 'user', content: `I am writing a CV, my details are `
       + `\n name: ${fullName} `
       + `\n role: ${currentPosition} (${currentLength} years). `
       + `\n I write in the technologies: ${currentTechnologies}. `
-      + `Can you write 10 points for a CV on what I am good at?`;
-    /** The job achievements prompt */
-    const prompt3 = `I am writing a CV, my details are`
+      + `Can you write 10 points for a CV on what I am good at?` };
+    /** The job achievements message */
+    const message3 = { role: 'user', content: `I am writing a CV, my details are`
       + `\n name: ${fullName}`
       + `\n role: ${currentPosition} (${currentLength} years).`
       + `\n During my years I worked at ${workArray.length} companies.${remainderText}`
       + `\n Can you write me 50 words for each company seperated in numbers of my succession `
-      + `in the company (in first person)?`;
+      + `in the company (in first person)?` };
     /** Generate a GPT-3 result */
-    console.log('Sending OpenAI request prompt1...')
-    const objective = await GPTFunction(prompt1);
-    await waitForMin();
-    console.log('Sending OpenAI request prompt2...')
-    const keypoints = await GPTFunction(prompt2);
-    await waitForMin();
-    console.log('Sending OpenAI request prompt3...')
-    const jobResponsibilities = await GPTFunction(prompt3);
+    const messages = [message0, message1, message2, message3]
+    console.log('Sending OpenAI request message1...')
+    const objective = await GPTFunction(messages);
+    // await waitForMin();
+    // console.log('Sending OpenAI request message2...')
+    // const keypoints = await GPTFunction(message2);
+    // await waitForMin();
+    // console.log('Sending OpenAI request message3...')
+    // const jobResponsibilities = await GPTFunction(message3);
     /** Put them into an object */
-    const chatGptData = { objective, keypoints, jobResponsibilities };
+    const chatGptData = { objective } ///, keypoints, jobResponsibilities };
     /** Log the result */
     console.log(chatGptData);
     res.json({
