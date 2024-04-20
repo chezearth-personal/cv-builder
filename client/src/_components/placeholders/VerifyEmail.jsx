ff06c1b248c3db56c6ad7d92c3843ddc6ccb3c81
Charles Rethman <rethman@me.com>
2024-04-20 19:45:06 +1200

    removed a lot of logs

--- client/src/_components/placeholders/VerifyEmail.jsx
+++ client/src/_components/placeholders/VerifyEmail.jsx
@@ -1,43 +1,6 @@
 import { Routes, Route, useParams } from 'react-router-dom';
 import { useState, useEffect, useRef } from 'react';
 import { fetchWrapper } from '../../_helpers/fetch-wrapper';
-// import {set} from 'react-hook-form';
-
-// const VerificationResponse = ({ url, verified, setVerified, response, setResponse }) => {
-  // console.log('Loading the VerificationResponse component ...');
-  // console.log('1. verified =', verified);
-  // const verifyEmail = async (url, verified, response) => {
-    // console.log('Running the verifyEmail() async function ...');
-    // try {
-      // console.log('2. verified =', verified);
-      // if (!verified) {
-        // // setVerified(true);
-        // // console.log('2. verified =', verified);
-        // const res = await fetchWrapper.get(url);
-        // console.log('API call completed.');
-        // console.log('response.status =', response && response.status);
-        // console.log('3. verified =', verified);
-        // setResponse({ ...response, ...(res || {}) });
-      // }
-    // } catch (error) {
-      // console.error('error =', error);
-      // // return { ...initialResponse, message: error.message };
-      // setResponse({ ...response, message: error.message });
-    // }
-  // }
-  // console.log('4. verified =', verified);
-  // verifyEmail(url, verified, response);
-  // useEffect(() => {
-    // console.log('5. verified =', verified);
-    // setVerified(true);
-    // console.log('6. verified =', verified);
-  // }, []);
-  // return (
-    // <div>
-      // <h1>{response.message}</h1>
-    // </div>
-  // );
-// }
 
 export const VerifyEmail = () => {
   console.log('Loading the component ...');
@@ -51,60 +14,36 @@ export const VerifyEmail = () => {
   /** State to handle useEffect() hook being called twice in render (for dev builds) */
   /** persists inside render but gets reset on each render */
   const ref = useRef(false);
-  // const localVerified = [ false ];
   /** State to persist over a whole render */
-  // const [ verified, setVerified ] = useState(false);
   /** Response from verification API call */ 
   const [ response, setResponse ] = useState(initialResponse);
   /** The API call to verify the email */
-  // console.log('1. verified =', verified);
-  console.log('1. ref.current =', ref.current);
+  // console.log('1. ref.current =', ref.current);
   const verifyEmail = async () => {
     console.log('Running the verifyEmail() async function ...');
     try {
-      // console.log('2. verified =', verified);
-      console.log('2. ref.current =', ref.current);
-      // console.log('1. localVerified =', localVerified[0]);
+      // console.log('2. ref.current =', ref.current);
       if (!ref.current) {
-      // if (!verified && !ref.current) {
-      // if (!verified && !localVerified[0]) {
-        // setVerified(true);
         ref.current = true;
-        // localVerified[0] = true;
-        // console.log('3. verified =', verified);
-        console.log('3. ref.current =', ref.current);
-        // console.log('2. localVerified[0] =', localVerified[0]);
+        // console.log('3. ref.current =', ref.current);
         const response = await fetchWrapper.get(url);
-        console.log('API call completed.');
+        // console.log('API call completed.');
         console.log('response.status =', response && response.status);
         setResponse({ ...initialResponse, ...(response || {}) });
-        // console.log('4. verified =', verified);
-        console.log('4. ref.current =', ref.current);
+        // console.log('4. ref.current =', ref.current);
       }
     } catch (error) {
       console.error('error =', error);
       setResponse({ ...initialResponse, message: error.message });
     }
   }
-  // const handleVerificationCall = () => {
-    // setResponse(verifyEmail(url, verified, response));
-    // return response;
-  // }
   useEffect(() => {
     console.log('Running the useEffect() hook ...');
-    // console.log('5. verified =', verified);
-    console.log('5. ref.current =', ref.current);
-    // setVerified(true);
+    // console.log('5. ref.current =', ref.current);
     verifyEmail();
-    // console.log('6. verified =', verified);
-    console.log('6. ref.current =', ref.current);
-    // console.log('3. localVerified =', localVerified[0]);
+    // console.log('6. ref.current =', ref.current);
   }, []);
-  // setVerified(true);
-  // console.log('7. verified =', verified);
-  console.log('7. ref.current =', ref.current);
-  // console.log('4. localVerified =', localVerified[0]);
-  console.log('(Re)Painting the screen ...');
+  // console.log('(Re)Painting the screen ...');
   
   return (
     <div>
