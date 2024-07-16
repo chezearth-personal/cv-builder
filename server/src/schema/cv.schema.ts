e60fe7e569210a462058436f4fe0323a53ca2b6b
Charles Rethman <rethman@me.com>
2024-07-17 09:03:12 +1200

    Updated cv.schema with type

--- server/src/schema/cv.schema.ts
+++ server/src/schema/cv.schema.ts
@@ -1,12 +1,14 @@
 import { object, array, string, TypeOf, z } from 'zod';
 
 export const CvSchema = object({
-  fullName: string({ required_error: 'Full name is required' }),
-  occupation: string({ required_error: 'Occupation is required' }),
-  tel: string({ required_error: 'Telephone number is required' }),
-  email: string({ required_error: 'Email is required' }),
-  website: string(),
-  skillTopics: array(object({ name: string()})),
-  companyDetails: array(object({ name: string()}))
+  body: object({
+    fullName: string({ required_error: 'Full name is required' }),
+    occupation: string({ required_error: 'Occupation is required' }),
+    tel: string({ required_error: 'Telephone number is required' }),
+    email: string({ required_error: 'Email is required' }),
+    website: string(),
+    skillTopics: array(object({ name: string()})),
+    companyDetails: array(object({ name: string()}))
+  })
 });
-export type Cv = TypeOf<typeof CvSchema>;
+export type Cv = TypeOf<typeof CvSchema>['body'];
